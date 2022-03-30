de78f22d7a88067a8690fa822e5c97695cfc0a72
enriquetomasmb <enriquetomasmb@gmail.com>
2022-03-30 19:25:46 +0200

    Add data acquisition thread

--- framework_acquisition.py
+++ framework_acquisition.py
@@ -4,22 +4,27 @@ HOST = "127.0.0.1"
 PORT = 5555
 BUFFER_SIZE = 4096
 
-print("Starting acquisition module...")
-print("Server running", HOST, PORT)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with open("rawdata.csv", "wb") as f:
-        print(f"Connected by {addr}")
-        while True:
-            bytes_read = conn.recv(BUFFER_SIZE)
-            if not bytes_read:
-                print("No data received")
-                break
-            f.write(bytes_read)
-    f.close()
-    conn.close()
-s.close()
+def acquire_signals():
+    print("Starting acquisition module...")
+    print("Server running", HOST, PORT)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with open("rawdata.csv", "wb") as f:
+            print(f"Connected by {addr}")
+            while True:
+                bytes_read = conn.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    print("No data received")
+                    break
+                f.write(bytes_read)
+        f.close()
+        conn.close()
+    s.close()
+
+if __name__ == "__main__":
+    acquire_signals()
 
