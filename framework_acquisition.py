1f15bf35bd4661c86f04a0a0a1b83b64f4e79335
enriquetomasmb <enriquetomasmb@gmail.com>
2022-03-31 14:24:03 +0200

    Fix thread and add data storage with file encryption functionality

--- framework_acquisition.py
+++ framework_acquisition.py
@@ -5,17 +5,24 @@ PORT = 5555
 BUFFER_SIZE = 4096
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with open("rawdata.csv", "wb") as f:
-        print(f"Connected by {addr}")
-        while True:
-            bytes_read = conn.recv(BUFFER_SIZE)
-            if not bytes_read:
-                print("Finished file transfer")
-                break
-            f.write(bytes_read)
-    f.close()
-    conn.close()
+def acquire_signals():
+    print("[+] Acquiring signals...")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with open("rawdata.csv", "wb") as f:
+            print(f"Connected by {addr}")
+            while True:
+                bytes_read = conn.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    print("Finished file transfer")
+                    break
+                f.write(bytes_read)
+        f.close()
+        conn.close()
+        print("File transfer complete")
+
+
+if __name__ == "__main__":
+    acquire_signals()
